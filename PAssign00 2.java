dbdb910bfd88c7989f53c030ca3b10c9dde72784
kelum1115 <ckokoyeswag@gmail.com>
2024-12-19 12:28:52 -0500

    Add files via upload

---
+++ PAssign00 2.java
@@ -0,0 +1,19 @@
+/**
+ * File: PAssign00
+ * Class: CSCI 1301
+ * Author: Christopher Williams
+ * Created on: Jun 5, 2017
+ * last Modified: Aug 18, 2019
+ * Description: Display a string value to the console
+ */
+
+public class PAssign00 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is Programming Assignment 0 for CSCI 1301.");
+		System.out.println("I have written my first \"official program\" and it works!");
+		System.out.println("Outputting text is not that hard, and I should make sure I get good at it.");
+	}
+
+}
